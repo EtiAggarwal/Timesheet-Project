5d55ee3459290bbfe0b89c2d18ac2d95cd08f82e
EtiAggarwal <aggarwaleti@gmail.com>
2015-10-07 00:45:57 -0400

    second commit through VS

--- TimeSheet/APP_CODE/DAL/DataAccessLayer.cs
+++ TimeSheet/APP_CODE/DAL/DataAccessLayer.cs
@@ -11,7 +11,7 @@ using System.Linq;
 using System.Web;
 
 /// <summary>
-/// Summary description for DataAccessLayetr
+/// Summary description for DataAccessLayers
 /// </summary>
 public class DataAccessLayer
 {
